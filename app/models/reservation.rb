33809e2b7cf4d8044e88864d016fa125fa78b4dd
Dave Broomhead <dave.broomhead@gmail.com>
2022-02-15 20:13:16 +1030

    seed database with tables

---
+++ app/models/reservation.rb
@@ -0,0 +1,6 @@
+class Reservation < ApplicationRecord
+
+    has_and_belongs_to_many :users
+    has_and_belongs_to_many :flights
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,93 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating Airplanes............"
+
+Airplane.destroy_all
+
+a1 = Airplane.create!(
+    name: 'Roo',
+    row: 30,
+    column: 4
+)
+a2 = Airplane.create!(
+    name: 'Enterprise',
+    row: 25,
+    column: 4
+)
+a3 = Airplane.create!(
+    name: 'Longreach',
+    row: 20,
+    column: 4
+)
+
+puts "Done. Created #{Airplane.count} planes."
+
+
+##################################################
+
+puts "Creating flights............"
+
+f1 = Flight.create!(
+    flight_number: 300,
+    origin: 'SYD',
+    destination:  'LAX',
+    date: '2022/02/18'
+)
+f2 = Flight.create!(
+    flight_number: 400,
+    origin: 'SYD',
+    destination:  'LHR',
+    date: '2022/02/18'
+)
+f3 = Flight.create!(
+    flight_number: 500,
+    origin: 'SYD',
+    destination:  'AKL',
+    date: '2022/02/18'
+)
+
+
+puts "Done. Created #{Flight.count} flights."
+
+
+##################################################
+
+
+puts "Creating users......."
+
+User.destroy_all
+
+u1 = User.create!(
+    name: 'Cameron',
+    is_admin: true,
+    password: 'chicken'
+) 
+u2 = User.create!(
+    name: 'David',
+    is_admin: true,
+    password: 'chicken'
+) 
+u3 = User.create!(
+    name: 'Jia',
+    is_admin: true,
+    password: 'chicken'
+) 
+
+puts "Done. Created #{User.count} users."
+
+
+##################################################
+
+
+puts "Creating bookings....."
+
+Reservation.destroy_all
+
+r1 = Reservation.create!()
+r2 = Reservation.create!()
+r3 = Reservation.create!()
+
+
+
