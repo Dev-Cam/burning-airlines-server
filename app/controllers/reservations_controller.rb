1ee5f4b22a4eb9635bae7bbb713a176288565356
Jia Song <jiasong214@gmail.com>
2022-02-16 13:02:39 +1100

    create API for flights and reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -20,4 +20,23 @@ class ReservationsController < ApplicationController
   def show
     @reservation = Reservation.find params[:id]
   end
+
+  ######################### API methods ###########################
+  def getAll
+    headers['Access-Control-Allow-Origin'] = '*'
+    render json: Reservation.all
+  end
+
+  def getAllUsers
+    headers['Access-Control-Allow-Origin'] = '*'
+
+    reservation = Reservation.where(user_id: params[:user_id])
+
+    if reservation.present?
+      render json: reservation
+    else
+      # TODO: need to change status
+      render json: {error: 'could not find reservation'}, status: 400
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,11 @@ Rails.application.routes.draw do
   resources :users
   resources :airplanes
 
+  # API endpoints 
+  get '/api/flights' => 'flights#getAll'
+  get '/api/flight/:id' => 'flights#getOne'
+
+  get '/api/reservations' => 'reservations#getAll'
+  get '/api/:user_id/reservations' => 'reservations#getAllUsers'
+
 end
